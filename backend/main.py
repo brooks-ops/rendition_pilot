cfcb4ae9b79d4c290181c828da8dbdad7e7a6f46
Brooks Barrett <brooks@LubbockCAD.com>
2026-04-29 16:24:41 -0500

    Fix CORS + local server setup, frontend successfully calling backend

--- backend/main.py
+++ backend/main.py
@@ -29,6 +29,8 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
         "null",
     ],
     allow_credentials=True,
